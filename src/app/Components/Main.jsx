15994eb15d270561b5ed707a382e43d8cedcc807
Edarth002 <arthuronyeanusi@gmail.com>
2025-05-26 21:18:26 +0100

    Update Main.jsx

--- src/app/Components/Main.jsx
+++ src/app/Components/Main.jsx
@@ -130,12 +130,17 @@ export const Main = () => {
 <script type="text/javascript">
     window.__be = window.__be || {};
     window.__be.id = "66c3555c86ccab0007190659";
+
     (function() {
-        var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
-        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.chatbot.com/widget/plugin.js';
-        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(be, s);
+        var be = document.createElement('script');
+        be.type = 'text/javascript';
+        be.async = true;
+        be.src = 'https://cdn.chatbot.com/widget/plugin.js'; // Force HTTPS
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(be, s);
     })();
 </script>
+
 <noscript>You need to <a href="https://www.chatbot.com/help/chat-widget/enable-javascript-in-your-browser/" rel="noopener nofollow">enable JavaScript</a> in order to use the AI chatbot tool powered by <a href="https://www.chatbot.com/" rel="noopener nofollow" target="_blank">ChatBot</a></noscript>
     </div>
   );
